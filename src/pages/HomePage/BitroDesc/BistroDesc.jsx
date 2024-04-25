7612fb57b202a70d5099aadb3464aefedf00cd0f
jubayer2023 <jubayersarker400@gmail.com>
2024-04-25 19:56:49 +0600

    bistro-desc-component-updated

--- src/pages/HomePage/BitroDesc/BistroDesc.jsx
+++ src/pages/HomePage/BitroDesc/BistroDesc.jsx
@@ -5,14 +5,14 @@ const BistroDesc = () => {
     <div className="bistro_box w-full h-full">
       {/* <img src={chefServiceImage} alt="chef" className="h-full  bg-fixed" /> */}
       <div
-        className="w-full h-full py-28"
+        className="w-full h-full py-24 md:py-28"
         style={{
           backgroundColor: "rgba(0, 0, 0, 0.6)",
           opacity: "1",
         }}
       >
-        <div className="flex justify-center items-center h-full max-w-screen-md mx-auto ">
-          <div className="text-center bg-white py-12 space-y-4">
+        <div className="flex justify-center items-center h-full w-3/4 md:max-w-screen-sm lg:max-w-screen-md mx-auto ">
+          <div className="text-center bg-white py-5 md:py-12 space-y-4">
             <h3 className="uppercase text-3xl">Bistro Boss</h3>
             <p className="px-10">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat,
